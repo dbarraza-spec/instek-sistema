9a5fd7c162f3c47369160de7c2a790942e94ef66
Daniel <d.barraza@instgps.com>
2025-08-05 19:27:43 -0700

    nuevo

--- run.py
+++ run.py
@@ -2,10 +2,13 @@ from app import create_app, db
 from flask_login import LoginManager
 from app.models import User
 
-app = create_app()  # ¡Esta línea define la variable app!
+# Esta variable 'app' DEBE existir en el nivel global
+app = create_app()
 
+# Crea las tablas de la base de datos si no existen
 with app.app_context():
     db.create_all()
 
+# Solo para desarrollo local, NO se usa en producción con Gunicorn
 if __name__ == '__main__':
     app.run(debug=True)
