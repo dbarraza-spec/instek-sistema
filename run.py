aa067e59361a7d5c28898d5a3c3d905a72c9ffbc
Daniel <d.barraza@instgps.com>
2025-08-05 19:22:03 -0700

    nuevo

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from app import create_app, db
 from flask_login import LoginManager
 from app.models import User
 
-app = create_app()
+app = create_app()  # ¡Esta línea define la variable app!
 
 with app.app_context():
     db.create_all()
