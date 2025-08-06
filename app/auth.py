b0fa539a3023eaf2d433ee41cb098be80c05bea0
Daniel <d.barraza@instgps.com>
2025-08-05 19:04:29 -0700

    d

---
+++ app/auth.py
@@ -0,0 +1,43 @@
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from .models import User
+from . import db
+from flask_login import login_user, logout_user, login_required
+from werkzeug.security import generate_password_hash, check_password_hash
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(email=email).first()
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('main.dashboard'))
+        else:
+            flash('Credenciales inválidas')
+    return render_template('login.html')
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        email = request.form['email']
+        name = request.form['name']
+        password = request.form['password']
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Correo ya registrado')
+        else:
+            new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Usuario creado exitosamente')
+            return redirect(url_for('auth.login'))
+    return render_template('register.html')
